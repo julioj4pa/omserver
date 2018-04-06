23e3af72ae3f79ee13c2ae131ad7d3a5a292991d
Thiago Ferezim <thiagoferezim@gmail.com>
2018-04-06 14:56:36 -0300

    set wedeploy config

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ var app = require('./config/server');
 var server = app.listen(3000, function(){
   console.log('::(omserver) Express server ON!');
   console.log('::(omserver) Ready on port %d', server.address().port);
+  // console.log(':: process.env.PORT' + process.env.PORT);
 });
