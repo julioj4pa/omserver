4b7b4eccc9f856f0cd7b4351ba6e8d79262b7088
Thiago Ferezim <thiagoferezim@gmail.com>
2018-04-28 08:28:36 -0300

    fix ProvidersDAO

--- app/models/ProvidersDAO.js
+++ app/models/ProvidersDAO.js
@@ -1,79 +1,52 @@
 var MongoClient = require('mongodb').MongoClient;
-var objectId = require('mongodb').ObjectId;
+var ObjectId = require('mongodb').ObjectId;
 
-function ProvidersDAO(){
-
-}
+function ProvidersDAO(){ }
 
 ProvidersDAO.prototype.insert = function(response, provider){
-  console.log('::ProvidersDAO.insert()');
   MongoClient.connect('mongodb://mongodb:27017', function(err, client){
-    console.log('::MongoClient connected');
     if(err) throw err;
-    // MongoNetworkError: failed to connect to server [localhost:27017]
-    // on first connect [MongoNetworkError: connect ECONNREFUSED 127.0.0.1:27017]
-
-    console.log('::no errors');
     var db = client.db('omdatabase');
     db.collection('providers').insert(provider, function(findErr, records){
       if(findErr) response.json({message: 'erro ao inserir dados'});
-      console.log('::records');
-      console.log(records);
       response.status(200).json({
-        message: 'fornecedor cadastrado com sucesso'
+        message: 'fornecedor cadastrado com sucesso',
+        records: records
       });
       client.close();
     });
   });
-
-
-  // this._connection.open(function(err, mongoclient){
-  //   mongoclient.collection('providers', function(err, collection){
-  //     collection.insert(provider, function(err, records){
-  //       console.log(records);
-  //       if(err){
-  //         response.json({message: 'erro ao inserir dados'});
-  //       } else {
-  //         response.status(200).json({
-  //           message: 'fornecedor cadastrado com sucesso'
-  //         });
-  //       }
-  //       mongoclient.close();
-  //     });
-  //   });
-  // });
 };
 
 ProvidersDAO.prototype.get = function(response, id){
-  if(id !== undefined){
-    id = {_id: objectId(id)};
-  }
-  this._connection.open(function(err, mongoclient){
-    mongoclient.collection('providers', function(err, collection){
-      collection.find(id).toArray(function(err, results){
-        if(err){
-          response.send({message: 'erro ao buscar dados'});
-        } else {
-          response.send({message: 'fornecedores encontrados', data: results});
-        }
-        mongoclient.close();
-      });
+  if(id !== undefined) id = {_id: ObjectId(id)};
+  MongoClient.connect('mongodb://mongodb:27017', function(err, client){
+    if(err) throw err;
+    var db = client.db('omdatabase');
+    db.collection('providers').find(id).toArray(function(findErr, results){
+      if(err){
+        response.send({message: 'erro ao buscar dados'});
+      } else {
+        response.send({message: 'fornecedores encontrados', data: results});
+      }
+      client.close();
     });
   });
+
 };
 
 ProvidersDAO.prototype.remove = function(response, id){
-  id = {_id: objectId(id)};
-  this._connection.open(function(err, mongoclient){
-    mongoclient.collection('providers', function(err, collection){
-      collection.remove(id, function(err, results){
-        if(err){
-          response.send({message: 'erro ao buscar dados'});
-        } else {
-          response.send({message: 'fornecedores encontrados', data: results});
-        }
-        mongoclient.close();
-      });
+  if(id !== undefined) id = {_id: ObjectId(id)};
+  MongoClient.connect('mongodb://mongodb:27017', function(err, client){
+    if(err) throw err;
+    var db = client.db('omdatabase');
+    db.collection('providers').remove(id, function(findErr, results){
+      if(err){
+        response.send({message: 'erro ao remover dados'});
+      } else {
+        response.send({message: 'fornecedor removido com sucesso', data: results});
+      }
+      client.close();
     });
   });
 };
